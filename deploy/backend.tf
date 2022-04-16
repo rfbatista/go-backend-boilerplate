c570f37fe34207145a2cd3f0fd06513c59a1f3fa
rfbatista <renfbatista@gmail.com>
2022-04-15 23:14:57 -0300

    update backend terraform

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,26 +1,15 @@
-variable "HASHICORP_ORGANIZATION_NAME" {
-	type = string
-	description = "Name of the organization containing the target workspace"
-}
-
-variable "HASHICORP_WORKSPACE_NAME" {
-	type = string
-	description = "The full name of one remote workspace"
-}
-
-variable "HASHICORP_TOKEN" {
-	type = string
-	description = "The token used to authenticate with the remote backend"
-}
-
 terraform {
-	cloud {
-		organization = var.HASHICORP_ORGANIZATION_NAME
-		token = var.HASHICORP_TOKEN
-		workspaces {
-			name = var.HASHICORP_WORKSPACE_NAME
-		}
-	}
-
-	required_version = ">= 0.13.0"
+  cloud {
+    organization = "kgen"
+    workspaces {
+      name = "portfolio"
+    }
+  }
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.6.0"
+    }
+  }
+  required_version = ">= 0.13.0"
 }
